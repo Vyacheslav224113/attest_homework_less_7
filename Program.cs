690fe2a430c25bd0f9b300c0455f42a16476ff50
Slava Korolev <vyacheslav.korolev.ru@mail.ru>
2024-02-28 23:32:29 +0700

    Task_1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+
+// void ShowNumbers(int start, int end)
+// {
+//     if (start == end)
+//     {
+//         Console.Write(start);
+//         return;
+//     }
+//     Console.Write(start + " ");
+//     ShowNumbers(start + 1, end);
+// }
+// ShowNumbers(4, 10);
+
